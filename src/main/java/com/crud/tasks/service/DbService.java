1b6196ffabc1b6fa5d8e2bc65a55084d333df8c0
Dariusz Grala <gralusek@interia.pl>
2021-03-28 12:08:16 +0200

    Task 19.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -20,7 +20,19 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Optional<Task> findById(Long id){
+/*    public Optional<Task> findById(Long id){
         return repository.findById(id);
+    }*/
+
+    public Optional<Task> getTask(final Long id) {
+        return repository.findById(id);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(final Long id) {
+        repository.deleteById(id);
     }
 }
