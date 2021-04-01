54851dbde5495debc9b4344540a6eef88e7b0f60
Dariusz Grala <gralusek@interia.pl>
2021-04-01 13:35:10 +0200

    Task 21.4

--- showtasks.bat
+++ showtasks.bat
@@ -1,10 +1,10 @@
-call runcrud
+call runcrud.bat
 if "%ERRORLEVEL%" == "0" goto showTasks
 echo.
 echo Runcrud has errors - breaking work
 goto fail
 
-:showtasks
+:showTasks
 echo.
 call "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8080/crud/v1/task/getTasks
 echo Tasks are shown.
