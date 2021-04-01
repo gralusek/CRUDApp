eaeb07cf36da318ded8214929ea82067a21b9d2e
Dariusz Grala <gralusek@interia.pl>
2021-04-01 13:26:25 +0200

    Task 21.4

---
+++ showtasks.bat
@@ -0,0 +1,19 @@
+call runcrud
+if "%ERRORLEVEL%" == "0" goto showTasks
+echo.
+echo Runcrud has errors - breaking work
+goto fail
+
+:showtasks
+echo.
+call "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8080/crud/v1/task/getTasks
+echo Tasks are shown.
+goto end
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,13 +2,18 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class TasksApplication {
+@SpringBootApplication
+public class TasksApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources(TasksApplication.class);
+	}
 }
